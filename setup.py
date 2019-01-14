d6cf45baadb9572d000122e597b2e12128b68482
jmartinezaguilar <Javier.Martinez@imb-cnm.csic.es>
2019-01-14 16:12:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ install_requires = ['numpy',
                     'matplotlib',
                     'quantities>=0.12',
                     'scipy',
-                    'neo=0.6.1',
+                    'neo==0.6.1',
                     'nixio',
                     'deepdish',
                     'PhyREC'
