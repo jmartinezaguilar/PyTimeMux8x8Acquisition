3786acf57993c73bef33cdd3c92962256da8120b
aguimera <anton.guimera@csic.es>
2019-02-08 10:54:17 +0100

    testing real time

---
+++ Tests/Real_Time_plot_and_save.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 12:19:57 2018
+
+@author: user
+"""
+
+import matplotlib.pylab as plt
+import numpy as np
+import time
+from PhyREC.NeoInterface import NeoSegment, NeoSignal
+import PhyREC.PlotWaves as Rplt
+import quantities as pq
+from itertools import  cycle
+
+AxesProp = {
+            'ylim': (-1, 1),
+            'facecolor': '#FFFFFF00',
+            'autoscaley_on': True,
+            'xaxis': {'visible': False,
+                      },
+            'yaxis': {'visible': False,
+                      },
+            'ylabel': '',
+            'title': None,
+            }
+
+FigProp = {'tight_layout': True,
+           'size_inches': (10,5),
+#               'facecolor': '#FFFFFF00',
+           }
+
+
+if __name__ == '__main__':
+    plt.close('all')
+
+    plt.rcParams.update({'axes.grid': True})
+    
+    # Variable inputs
+    Fs = float(2e3)
+    Ts = 1/Fs
+    Fsig = 100    
+    ReBufferSize = 3000
+    nIters = ReBufferSize * 20
+    nChannels = 8
+    
+    Pcycle = np.round(Fs/Fsig)
+    Fsig = Fs/Pcycle
+    
+    t = np.linspace(0, 1/Fsig, Pcycle)
+    
+    samples = np.sin(2*np.pi*Fsig*t)
+    
+    plt.plot(t, samples)
+    
+#    np.sin()
+
+#    # Create channel names
+#    ChannelNames = []
+#    Columns = sorted(doColumns.keys())
+#    for RowName in sorted(aiChannels.keys())[0:nRows]:
+#        for ColName in sorted(doColumns.keys())[0: nColumns]:
+#            ChannelNames.append(RowName+ColName)
+#
+#    # Init Recording to store the data
+#    RecOut = NeoSegment()
+#    for ChName in ChannelNames:
+#        sig = NeoSignal(np.array([]),
+#                        units=pq.V,
+#                        t_start=0*pq.s,
+#                        sampling_rate=FsCh*pq.Hz,
+#                        name=ChName)
+#        RecOut.AddSignal(sig)
+#
+#    # Init figure
+#    fig, ax = plt.subplots(nColumns, nRows, sharex=True)
+#    ax = ax.flatten()
+#    Slots = []
+#    for isl, sig in enumerate(RecOut.Signals()):
+#        Slots.append(Rplt.WaveSlot(sig,
+#                                   Ax=ax[isl],
+#                                   ))
+#    PltSl = Rplt.PlotSlots(Slots,
+#                           Fig=fig,
+#                           AxKwargs=AxesProp,
+#                           FigKwargs=FigProp,
+#                           TimeAxis=None)
+#    PltSl.PlotChannels(None)
+#
+#    # Generate digital lines, sorting indexes and dummy samples
+#    DigLines, SortDInds = GenDigitalLines(nColumns=nColumns)
+#    DummySamps = GenDummySamples(nColumns=nColumns,
+#                                 nRows=nRows,
+#                                 nSampsCh=nSampsCh)
+#
+#    # Define process buffer
+#    RefreshBuffer = np.zeros((ReBufferSize, nColumns*nRows))
+#    BufferInd = 0
+#
+#    # Start timer
+#    Tstart = time.time()
+#
+#    ##
+##    f = open('TstRealTimeEval.dat', 'wb')
+##    np.savetxt(f, [])
+#    ##
+#    for i in range(nIters):
+##        DummyChSamps = SortingData_list(DigLines=DigLines,
+##                                        Samps=DummySamps)
+#        # Sorting input data
+##        DummyChSamps = SortingData_list2(SortDInds=SortDInds,
+##                                         Samps=DummySamps)
+#        DummyChSamps = SortingData_list3(SortDInds=SortDInds,
+#                                         Samps=DummySamps,
+#                                         nCols=nColumns,
+#                                         nRows=nRows,
+#                                         nSampsCh=nSampsCh)
+#        
+#        # Store data in plealocated buffer
+#        Sample = DummyChSamps.mean(axis=1)[None, :]
+#        RefreshBuffer[BufferInd, :] = Sample
+#        BufferInd += 1
+#
+#        # Update recording and plot data
+#        if BufferInd == ReBufferSize:
+#            for si, sn in enumerate(ChannelNames):
+#                RecOut.AppendSignal(sn,
+#                                    RefreshBuffer[:, si][:, None])
+#                sig = RecOut.GetSignal(sn)
+#            ##
+##            np.savetxt(f, RefreshBuffer)
+##            f.flush()
+#            ##
+#            BufferInd = 0
+#            RefreshBuffer = np.zeros((ReBufferSize, nColumns*nRows))
+#            for sl in PltSl.Slots:
+#                sl.Signal = RecOut.GetSignal(sl.name)
+#            PltSl.PlotChannels(None)
+#
+#    PltSl.AddLegend()
+#    Tend = time.time()
+#
+##    f.close()
+#
+#    AcqTime = Ts*nSampsCh*nColumns
+#    ProcTime = (Tend-Tstart)/nIters
+#    FsMax = 1/(ProcTime/(nSampsCh*nColumns))
+#    SwFreqMax = 1/((1/FsMax) * nSampsCh * nColumns)
+#    print('Process time -> ', ProcTime)
+#    print('Max Sampling -> ', FsMax)
+#    print('Max SwitchFreq -> ', SwFreqMax)
+#    print('Max RefreshFreq -> ', 1/((1/SwFreqMax)*ReBufferSize))
+#    print('Acquisition time -> ', AcqTime)
+#    print('TimeRefresh ->', (1/FsCh)*ReBufferSize)
+#    if AcqTime < ProcTime:
+#        print('BAD!!!!!!!!!!!')
+#    else:
+#        print('Good')
